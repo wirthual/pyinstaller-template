cbf141301f5d8bfdbc088218c2c7c5f316efddc8
raphael <raphael@dexterity.ai>
2020-04-11 14:06:19 -0700

    added changed main file to add pysimplegui

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import os
+import PySimpleGUI as sg
 
 
 def resource_path(relative_path):
@@ -18,4 +19,19 @@ if __name__=='__main__':
    with open(file_path) as f:
      data = json.load(f)
 
-   print(data['key'])
+   sg.theme('DarkAmber')   # Add a touch of color
+   # All the stuff inside your window.
+   layout = [  [sg.Text(data['key'])],
+                [sg.Text('Enter something on Row 2'), sg.InputText()],
+                [sg.Button('Ok'), sg.Button('Cancel')] ]
+
+   # Create the Window
+   window = sg.Window('Window Title', layout)
+   # Event Loop to process "events" and get the "values" of the inputs
+   while True:
+        event, values = window.read()
+        if event in (None, 'Cancel'):   # if user closes window or clicks cancel
+            break
+        print('You entered ', values[0])
+
+   window.close()
